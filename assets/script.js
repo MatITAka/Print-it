06429bd0433c79c996895ea83b21b50bb4d7026c
Matzoo38 <mslyemi@gmail.com>
2023-05-23 18:08:40 +0200

    sans bullet point

--- assets/script.js
+++ assets/script.js
@@ -16,3 +16,59 @@ const slides = [
 		"tagLine":"Autocollants <span>avec découpe laser sur mesure</span>"
 	}
 ]
+
+let i = 0;
+let last = slides.length - 1;
+
+const SlideImg = document.getElementById('Slide-Img');
+
+const suivant = () => {
+	i = i +1;
+	if( i === 4){
+		i = 0;
+	}
+	
+}
+
+const precedent = () => {
+	i = i -1;
+	if( i === -1){
+		i = 3;
+	}
+	
+}
+
+
+
+const arrowLeft= document.querySelector(".arrow_left")
+const arrowRight= document.querySelector(".arrow_right")
+const bannerText = document.querySelector('#banner > p');
+const DotSlide = document.querySelector(".dot")
+
+
+
+ arrowLeft.addEventListener('click', e =>{
+ 	console.dir("swipe left" + slides[i]+ [i]);
+ 	precedent();
+ 	SlideImg.src= "./assets/images/slideshow/" + slides[i].image;
+ 	bannerText.innerHTML = slides[i].tagLine;
+	 
+	
+ })
+
+ arrowRight.addEventListener('click' , e=>{
+ 	console.dir('swipe right'+ slides[i]+ [i]);
+ 	suivant()
+ 	SlideImg.src="./assets/images/slideshow/" + slides[i].image;
+ 	bannerText.innerHTML = slides[i].tagLine;
+
+})
+
+
+
+
+
+
+
+
+
